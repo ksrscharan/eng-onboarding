998227839eea19c28ed23ac2236324a4d49d2254
KSRS CHARAN <51869689+ksrscharan@users.noreply.github.com>
2022-06-20 17:52:00 +0530

    Added Function, Code Formatted

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const number = parseInt(prompt('Enter the number of terms: '));
-let n1 = 0, n2 = 1, nextTerm;
+function fibonacci(number) {
+  let arr = []
+  let n1 = 0,
+    n2 = 1,
+    nextTerm;
 
-console.log('Fibonacci Series:');
-for (let i = 1; i <= number; i++) {
-  console.log(n1);
-  nextTerm = n1 + n2;
-  n1 = n2;
-  n2 = nextTerm;
+  for (let i = 1; i <= number; i++) {
+    arr.push(n1)
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+  }
+  return arr
 }
+const number = parseInt(prompt("enter number"));
+
+fibonacci(number)
