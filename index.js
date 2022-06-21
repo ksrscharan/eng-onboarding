f50b736d559a480a4efacfcabfdd237094213729
KSRS CHARAN <51869689+ksrscharan@users.noreply.github.com>
2022-06-21 10:19:27 +0530

    format code with prettier

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 function printFibonacciNumbers(number) {
-    let arr = [];
-    let n1 = 0, n2 = 1, nextTerm;
+  let arr = [];
+  let n1 = 0,
+    n2 = 1,
+    nextTerm;
 
-    for (let i = 1; i <= number; i++) {
-        arr.push(n1);
-        nextTerm = n1 + n2;
-        n1 = n2;
-        n2 = nextTerm;
-    }
+  for (let i = 1; i <= number; i++) {
+    arr.push(n1);
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+  }
 
-    return arr;
+  return arr;
 }
 
-const number = parseInt(prompt("enter number of terms"));
+const number = parseInt(
+  prompt("Please enter number upto which you want fibonnaci series ")
+);
 
 printFibonacciNumbers(number);
